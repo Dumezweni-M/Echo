e2802b9b25dc0786041b4e7ff1968bfc664e8b72
Dumzi <105924691+Dumezweni-M@users.noreply.github.com>
2024-11-21 16:36:24 +0200

    Setup: Express server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 
 const PORT = 3000;
 
@@ -9,14 +9,37 @@ const morgan = require('morgan');
 const path = require('path');
 
 // Import models here
+console.log("Loading Model...");
+const Task = require('./models/task')
+console.log("Model Successfully loaded...");
+
+const app = express();
+
+// app.set('views', path.join(__dirname, 'views'));
 
 // Error handler middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("<----- Error MW ----->")
+});
 
 // Database Connection
+const dbURI = process.env.MONGO_URI;
 
-// Register View Engine
+mongoose.connect(dbURI)
+    .then(result => app.listen (PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }))
+    .catch(err => console.log("----- Mongoose Connection Issue -----", err))
+
+// Register View 
+// app.set('view engine'. 'ejs');
 
 // Middleware for folder permissions
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(express.json());
 
 // Retrieve Tasks from DB
 
