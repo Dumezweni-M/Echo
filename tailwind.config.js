f60d6a7478ca00af043c86ecead96670c1c7ae57
Dumzi <105924691+Dumezweni-M@users.noreply.github.com>
2024-11-24 21:31:21 +0200

    refactor: modularized each section of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html, js}", "./views/**/*.ejs"],
+  content: 
+    ["./public/**/*.{html, js}",
+     "./views/**/*.ejs"],
   theme: {
     extend: {},
   },
