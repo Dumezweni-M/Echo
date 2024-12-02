2ed9857a47d44123fc33f771175fa6928b3d057d
Dumzi <105924691+Dumezweni-M@users.noreply.github.com>
2024-12-02 13:19:39 +0200

    feat: Mobile view implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     ["./public/**/*.{html, js}",
      "./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamilu: {
+        custom: ["Silkscreen", 'sans-serif'], 
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   plugins: [],
 }
